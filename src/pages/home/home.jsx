d322aa6bab0e8cecc9a38c22e1bd854cf4502536
danielDj24 <danieldj242001@gmail.com>
2024-12-26 10:27:08 -0500

    add swiper

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,6 +3,8 @@ import Menu from "../../components/menu/menu";
 import Footer from "../../components/footer/footer";
 import BannerCarrousel from "../../components/carrousel/Bannercarrousel";
 import roseArrangementImage from "../../assets/Grupo -5.png"
+import saleImage from "../../assets/Grupo -8.png"
+import festImage from "../../assets/Grupo -9.png"
 
 const Home = () => {
     // Lista de diccionarios con los datos de los banners
@@ -31,11 +33,14 @@ const Home = () => {
         <div className="flex flex-col min-h-screen">
             <Menu />
             <div className="flex-grow">
-                {/* Aquí va el contenido principal */}
                 <div className="content">
-
-                    {/* Pasamos los banners como prop al componente BannerCarrousel */}
                     <BannerCarrousel banners={banners} />
+                        
+                    {/* Contenedor para las imágenes más pequeñas */}
+                    <div className="flex justify-center items-center space-x-8">
+                        <img src={saleImage} alt="Image 1" className="w-2/6 h-auto" />
+                        <img src={festImage} alt="Image 2" className="w-2/6 h-auto" />
+                    </div>
                 </div>
             </div>
             <Footer />
